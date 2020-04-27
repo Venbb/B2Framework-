d385d62f2daa50cab5f708c161d8f1cff19563b0
biao.wen <biao.wen@pitayagames.com>
2020-04-27 18:20:34 +0800

    添加UI节点绑定

---
+++ Assets/B2Framework/Examples/Variables/VariableExample.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using B2Framework.UI;
+public class VariableExample : MonoBehaviour
+{
+    public VariableList variables;
+}
